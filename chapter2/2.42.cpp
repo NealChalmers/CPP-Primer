34f618a36f4ade5c23a34b24bad7bbb8ab82b985
NealChalmers <Chalmers111@qq.com>
2018-04-16 18:15:26 +0800

    Update 2.42.cpp

--- chapter2/2.42.cpp
+++ chapter2/2.42.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	Salas_data data1, data2;
 	double price = 0;
-	std::cin >> data1.bookNo >> data1.units_sold >> price;			//ÖØ×öp67Á·Ï°
+	std::cin >> data1.bookNo >> data1.units_sold >> price;			//refactor p67 exercise
 	data1.revenue = data1.units_sold*price;
 	std::cin >> data2.bookNo >> data2.units_sold >> price;
 	data2.revenue = data2.units_sold*price;
