749e25f80c9e1cd8e93e1e02dc7cfffd5f31873d
NealChalmers <Chalmers111@qq.com>
2018-04-16 18:25:30 +0800

    Update 1.16.cpp

--- chapter1/1.16.cpp
+++ chapter1/1.16.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	int i, j = 0;
-	std::cout << "Input numbers, Ctrl + Z to end input" << std::endl;		//输出一组数的和
+	std::cout << "Input numbers, Ctrl + Z to end input" << std::endl;		// out put the sum of integers
 	while (std::cin >> i)
 		j += i;
 	std::cout << j << std::endl;
