7bb3d9a33db4470079a7f98a4052a54a29ef753c
NealChalmers <Chalmers111@qq.com>
2018-05-06 16:18:45 +0800

    chapter6

--- chapter6/6.12.cpp
+++ chapter6/6.12.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
-void swap(int *a, int *b)			//swap by pointer
+void swap(int &a, int &b)			//swap by pointer
 {
 	int c;
-	c = *a;
-	*a = *b;
-	*b = c;
+	c = a;
+	a = b;
+	b = c;
 }
 
 int main()
 {
 	int a = 2, b = 3;
-	swap(&a, &b);
+	swap(a, b);
 	cout << "a:" << a << " b:" << b;
 }
